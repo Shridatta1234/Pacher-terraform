d904efa547d258aa2dfbfec12904112556be0d51
Shridatta1234 <131602138+Shridatta1234@users.noreply.github.com>
2025-05-21 09:49:39 +0000

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "packer-instance" {
-  ami                    = var.ami_id
+  ami                    = var.ami_id != null ? var.ami_id : data.aws_ami.my_pta_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 

a23c96918844f4d2b24c1d48a446aba95153b748
Shridatta1234 <131602138+Shridatta1234@users.noreply.github.com>
2025-05-21 09:49:25 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,8 +10,20 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
+
+
+data "aws_ami" "my_pta_ami" {
+  most_recent = true
+  owners      = ["self"] # Or your AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["my-pta-ami-*"] # Match your Packer AMI naming pattern
+  }
+}
+
 variable "ami_id" {
   description = "AMI ID Custom with packer"
   type        = string
-  default     = "ami-0202cceb865dd6304" 
+  default     = null # Default to null to allow data source to be used
 }
